b1bd7fc90df742e3b4935847bbc8a941e42b16e5
ShadyPenguin <jakesikora.01@gmail.com>
2026-05-03 21:34:07 -0400

    consolidate suite tests

--- tests/test_failures.py
+++ tests/test_failures.py
@@ -4,10 +4,10 @@ import allure
 from penguin_tests import failure
 
 
-@allure.parent_suite("Failiure Parent Suite")
+@allure.parent_suite("App 1")
 @allure.suite("Failure Suite")
 @allure.sub_suite("Failure Sub Suite")
-class TestMain:
+class TestFailure:
     def test_failure(self):
         assert failure.return_false() is False
 
@@ -19,4 +19,22 @@ class TestMain:
         "This test is skipped"
     )  # pyright: ignore[reportUndefinedVariable]
     def test_skip(self):
+        assert False
+
+
+@allure.parent_suite("App 1")
+@allure.suite("Failure Suite")
+@allure.sub_suite("Failure Extra Sub Suite")
+class TestFailureExtra:
+    def test_failure2(self):
+        assert failure.return_false() is False
+
+    def test_error2(self):
+        failure.throw_exception()
+        assert False
+
+    @pytest.mark.skip(
+        "This test is skipped"
+    )  # pyright: ignore[reportUndefinedVariable]
+    def test_skip2(self):
         assert False
--- tests/test_main.py
+++ tests/test_main.py
@@ -7,30 +7,23 @@ from penguin_tests.main import main
 logger = logging.getLogger(__name__)
 
 
-@allure.parent_suite("Main Parent Suite")
+@allure.parent_suite("App 1")
 @allure.suite("Main Suite")
 @allure.sub_suite("Main Sub Suite")
+@allure.tag("owner-jake")
+@allure.tag("unit")
 class TestMain:
-    # @pytest.fixture(scope="module", autouse=True)
-    # def before_each():
-    #     allure.dynamic.parent_suite("'Main' Tests")
-    #     allure.dynamic.sub_suite("Jake")
-    #     allure.dynamic.label("owner", "Jake")
-    #     logger.error("oops")
-    #     yield
 
     @allure.title("Foo some Tests Bar!")
     def test_foobar(self):
         assert True is True
 
-    # @allure.parent_suite("All Tests")
-    # @allure.suite("Main")
-    # @allure.sub_suite("Integration")
+    @pytest.mark.integration
     def test_main(self):
         assert main() == "Hello, World!"
 
     # @allure.suite("Main")a
     # @allure.sub_suite("Smoke")
-    # @pytest.mark.integration
+    @pytest.mark.full
     def test_main_error(self):
         assert main() == "Hello, World!"
